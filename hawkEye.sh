002e2a29360bbbb4a3b7d0e1d7a2c1045f51336d
Teodora Carneanu <tcarneanu@smartspot.ro>
2018-10-11 16:32:22 +0300

    convert from UTF-16LE to UTF-8

--- hawkEye.sh
+++ hawkEye.sh
@@ -16,8 +16,15 @@ for i in `find $ZIP_DIR -type f -name *.zip`
 do
 	# unzip checks to current location /opt/hawkEye
 	unzip $i
+	
+	find /opt/hawkEye -size 0 -exec rm {} \;
 
-	find /opt/hawkEye -size 0
+	find /opt/hawkEye -maxdepth 1 -type f -name "SS*" -exec iconv -f UTF-16LE -t UTF-8 {} -o {} \;
+	
+	#for j in `find /opt/hawkEye -maxdepth 1 -type f -name "SS*"`
+	#do
+	#	iconv -f UTF-16LE -t UTF-8 $j -o $j
+	#done
 	# move files to processed directory in the end
 	#mv /opt/hawkEye/SS* $BAK_DIR
 
