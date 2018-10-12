4ad3a092dd0e0272ca5727d03114f48807d2573f
Teodora Carneanu <tcarneanu@smartspot.ro>
2018-10-12 10:29:28 +0300

    Declared variables for paths

--- hawkEye.sh
+++ hawkEye.sh
@@ -2,29 +2,26 @@
 
 ZIP_DIR="/opt/hawkEye/checks"
 BAK_DIR="/opt/hawkEye/checks/processed"
-
-unzip_check() {
-	unzip $1
-}
-
-move_to_processed() {
-	mv $1 $2
-}
+ZERO_DIR="/opt/hawkEye/checks/offlineMachines"
+WORKING_DIR="/opt/hawkEye/"
 
 
 for i in `find $ZIP_DIR -type f -name *.zip`
 do
 	# unzip checks to current location /opt/hawkEye
 	unzip $i
-	
-	find /opt/hawkEye -size 0 -exec rm {} \;
 
-	find /opt/hawkEye -maxdepth 1 -type f -name "SS*" -exec iconv -f UTF-16LE -t UTF-8 {} -o {} \;
+	# removes files with size 	
+	find $WORKING_DIR -size 0 -exec mv {} $ZERO_DIR \;
 	
-	#for j in `find /opt/hawkEye -maxdepth 1 -type f -name "SS*"`
-	#do
-	#	iconv -f UTF-16LE -t UTF-8 $j -o $j
-	#done
+	# convert from UTF-16LE to UTF-8
+	find $WORKING_DIR -maxdepth 1 -type f -name "SS*" -exec iconv -f UTF-16LE -t UTF-8 {} -o {} \;
+	
+	for j in `find $WORKING_DIR -maxdepth 1 -type f -name "SS*"`
+	do 
+		echo this is the filename
+		basename $j
+	done
 	# move files to processed directory in the end
 	#mv /opt/hawkEye/SS* $BAK_DIR
 
