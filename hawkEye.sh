14ca3d3232992b4dba84918a7ce3698d5f455588
Teodora Carneanu <tcarneanu@smartspot.ro>
2018-10-16 10:26:32 +0300

    Working on filtering checks 1

--- hawkEye.sh
+++ hawkEye.sh
@@ -5,6 +5,12 @@ BAK_DIR="/opt/hawkEye/checks/processed"
 ZERO_DIR="/opt/hawkEye/checks/offlineMachines"
 WORKING_DIR="/opt/hawkEye/"
 OS_SOFT="/opt/hawkEye/default/osSoftware.txt"
+OPS_ALLOWED="/opt/hawkEye/default/operations.txt"
+HR_ALLOWED="/opt/hawkEye/default/hr.txt"
+SALES_ALLOWED="opt/hawkEye/default/sales.txt"
+
+OPS=`mysql -u root -psolaris -D hawk_eye -e "SELECT * from user where empl_type='operations'"`
+
 
 for i in `find $ZIP_DIR -type f -name *.zip`
 do
@@ -16,13 +22,31 @@ do
 	
 	# convert from UTF-16LE to UTF-8
 	find $WORKING_DIR -maxdepth 1 -type f -name "SS*" -exec iconv -f UTF-16LE -t UTF-8 {} -o {} \;
-	
+
+	# filter SYS related software	
 	for j in `find $WORKING_DIR -maxdepth 1 -type f -name "SS*"`
 	do 
 		FILENAME=`basename $j`
 		grep -v -f $OS_SOFT  $WORKING_DIR$FILENAME > /tmp/$FILENAME; mv -f /tmp/$FILENAME $WORKING_DIR
 
 	done
+
+	# rename files 	
+        for j in `find $WORKING_DIR -maxdepth 1 -type f -name "SS*"`
+        do
+		BASENAME=`basename $j | awk -F "-" '{print $1}'`
+		mv $j $BASENAME
+        done
+
+	
+	for j in `find $WORKING_DIR -maxdepth 1 -type f -name "SS*"`
+	do
+		BASENAME=`basename $j`	
+		echo User with PC $j has the follwing type >> test.txt
+		EMPL_TYPE="SELECT empl_type FROM user WHERE laptop_id like '"$BASENAME"';"
+		mysql -u root -psolaris -D hawk_eye -e "$EMPL_TYPE" >> test.txt
+	done		
+	
 	# move files to processed directory in the end
 	#mv /opt/hawkEye/SS* $BAK_DIR
 
