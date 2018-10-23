63b7065685ac740fc320af97018cea1caf7ee280
Teodora Carneanu <tcarneanu@smartspot.ro>
2018-10-23 14:48:38 +0300

    Filter zip, send mail, provide stats v1

--- hawkEye.sh
+++ hawkEye.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MAIL_DIR="/opt/hawkEye/archive/"
 ZIP_DIR="/opt/hawkEye/checks"
 BAK_DIR="/opt/hawkEye/checks/processed"
 ZERO_DIR="/opt/hawkEye/checks/offlineMachines"
@@ -9,7 +10,10 @@ OPS_ALLOWED="/opt/hawkEye/default/operations.txt"
 HR_ALLOWED="/opt/hawkEye/default/hr.txt"
 SALES_ALLOWED="/opt/hawkEye/default/sales.txt"
 
-for i in `find $ZIP_DIR -type f -name *.zip`
+touch $WORKING_DIR/mail.txt
+touch $WORKING_DIR/offlineComputers.txt
+
+for i in `find $ZIP_DIR -maxdepth 1 -type f -name *.zip`
 do
 	# unzip checks to current location /opt/hawkEye
 	unzip $i &> /dev/null
@@ -19,8 +23,17 @@ do
 	echo Checked on $ARCHIVE_NAME >> mail.txt
 	echo "----------------------------------------------------------------------------------------" >> mail.txt
 
+	# compile a list of scans with zero size
+	for j in `find $WORKING_DIR -maxdepth 1 -size 0 -name "SS*"` 
+	do	
+		BASENAME=`basename $j | awk -F "-" '{print $1}'`
+		EMPL_NAME="SELECT name FROM user WHERE laptop_id like '"$BASENAME"';"
+                SQL_NAME=`mysql -u root -psolaris -D hawk_eye -s -e "$EMPL_NAME"`
+		echo "$SQL_NAME with $BASENAME has not been scanned on $ARCHIVE_NAME" >> offlineComputers.txt
+	done
+
 	# removes files with size 	
-	find $WORKING_DIR -size 0 -exec mv -f {} $ZERO_DIR \;
+	find $WORKING_DIR -maxdepth 1 -size 0 -exec mv -f {} $ZERO_DIR \;
 	
 	# convert from UTF-16LE to UTF-8
 	find $WORKING_DIR -maxdepth 1 -type f -name "SS*" -exec dos2unix -ascii {} \; 
@@ -81,13 +94,10 @@ do
 		elif [[ $SQL == "mgmt" ]]
 		then 
 			echo "$SQL_NAME is using $BASENAME" >> mail.txt
-			echo "The following programes installed " >> mail.txt
-			cat $j >> mail.txt
 			echo "----------------------------------------------------------------------------------------" >> mail.txt	
 		elif [[ $SQL == "anevia" ]]
 		then
 			echo "$SQL_NAME is using $BASENAME. Employee is part of team Anevia." >> mail.txt
-			cat $j >> mail.txt
 			echo "----------------------------------------------------------------------------------------" >> mail.txt
 		else
 			echo $BASENAME is not in DB >> mail.txt
@@ -98,6 +108,17 @@ do
 	zip `date +%F` SS*
 	rm -f SS*
 	mv *.zip $BAK_DIR
+
 	# remove archive from directory checks since it has been checked
-	#rm $i
+	mv $i $BAK_DIR
 done
+
+mysql -u root -psolaris -D hawk_eye -s -e "SELECT name, checkCount, totalChecks FROM user INTO OUTFILE '/var/lib/mysql-files/stats.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
+
+echo "Please find the results of the latest software scan" | mail -v -s "Hawk eye checks" -r "SmartSpot Software Scan<mailer@smartspot.ro>" -a mail.txt -a offlineComputers.txt -a /var/lib/mysql-files/stats.csv tcarneanu@smartspot.ro,sstruti@smartspot.ro
+
+zip `date +%F` mail.txt offlineComputers.txt /var/lib/mysql-files/stats.csv
+rm -f mail.txt offlineComputers.txt /var/lib/mysql-files/stats.csv
+mv *.zip $MAIL_DIR
+
+
